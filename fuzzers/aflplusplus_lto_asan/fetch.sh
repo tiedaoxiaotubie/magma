ef07d2df37f6305fbadfea49904a34b6f330e53f
tiedaoxiaotubie <weihaijiangzhiyuan@126.com>
2021-03-15 11:31:39 +0800

    fetch.sh: fix bug for poppler instrumentation failure

--- fuzzers/aflplusplus_lto_asan/fetch.sh
+++ fuzzers/aflplusplus_lto_asan/fetch.sh
@@ -9,3 +9,38 @@ set -e
 git clone --no-checkout https://github.com/AFLplusplus/AFLplusplus "$FUZZER/repo"
 git -C "$FUZZER/repo" checkout 068bef5eab942df0a133c92522f2ab81b28ac636
 
+# Fix: CMake-based build systems fail with duplicate (of main) or undefined references (of LLVMFuzzerTestOneInput)
+sed -i '{s/^int main/__attribute__((weak)) &/}' $FUZZER/repo/utils/aflpp_driver/aflpp_driver.c
+sed -i '{s/^int LLVMFuzzerTestOneInput/__attribute__((weak)) &/}' $FUZZER/repo/utils/aflpp_driver/aflpp_driver.c
+cat >> $FUZZER/repo/utils/aflpp_driver/aflpp_driver.c << EOF
+__attribute__((weak))
+int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
+{
+  // assert(0 && "LLVMFuzzerTestOneInput should not be implemented in afl_driver");
+  return 0;
+}
+EOF
+
+patch -p1 -d "$FUZZER/repo" << EOF
+--- a/utils/aflpp_driver/aflpp_driver.c
++++ b/utils/aflpp_driver/aflpp_driver.c
+@@ -111,7 +111,8 @@ extern unsigned int * __afl_fuzz_len;
+ __attribute__((weak)) int LLVMFuzzerInitialize(int *argc, char ***argv);
+ 
+ // Notify AFL about persistent mode.
+-static volatile char AFL_PERSISTENT[] = "##SIG_AFL_PERSISTENT##";
++// DISABLED to avoid afl-showmap misbehavior
++static volatile char AFL_PERSISTENT[] = "##SIG_AFL_NOT_PERSISTENT##";
+ int                  __afl_persistent_loop(unsigned int);
+ 
+ // Notify AFL about deferred forkserver.
+--- a/src/afl-showmap.c
++++ b/src/afl-showmap.c
+@@ -1021,6 +1021,7 @@ int main(int argc, char **argv_orig, char **envp) {
+     if (arg_offset && use_argv[arg_offset] != stdin_file) {
+ 
+       use_argv[arg_offset] = strdup(stdin_file);
++      fsrv->out_file = use_argv[arg_offset];
+ 
+     }
+EOF
